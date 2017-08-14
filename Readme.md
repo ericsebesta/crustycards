d86d340651ad454b32356e2126d6357741bf8b02
A B <ab@gmail.com>
2017-08-13 20:05:57 -0500

    split out code into more function, add some tests

--- Readme.md
+++ Readme.md
@@ -3,6 +3,4 @@ CrustyCards - a Hearthstone Card interface in Rust
 JSON data from https://hearthstonejson.com/
 
 TODO:
-check into git
-refactor load from parse
-add tests
+convert json to card types
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate serde_json;
 
 use serde_json::{Value, Error};
@@ -5,24 +6,63 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
-fn load_json(file_relative_path: &str) -> io::Result<Value> {
+fn load_json_from_file(file_relative_path: &str) -> io::Result<String> {
     let mut f = File::open(file_relative_path)?;
     let mut buffer = String::new();
     f.read_to_string(&mut buffer)?;
-    let v: Value = serde_json::from_str(&buffer)?;
-    Ok(v)
+    Ok(buffer)
+}
+
+fn parse_json_to_values(buffer: &str) -> Result<Value, String> {
+    match serde_json::from_str(buffer) {
+        Ok(value) => Ok(value),
+        Err(e) => Err(e.to_string()),
+    }
 }
 
 fn main() {
-    let result = load_json("data/cards.collectible.json");
-    match result {
-        Ok(value) => {
-            let pretty_result = serde_json::to_string_pretty(&value);
-            match pretty_result {
-                Ok(v) => println!("Data is: {}", v),
-                Err(e) => println!("Couldn't pretty print: {}", e),
+    let json_as_string_result = load_json_from_file("data/cards.collectible.json");
+    match json_as_string_result {
+        Ok(json_as_string) => {
+            let json_as_value_result = parse_json_to_values(&json_as_string);
+            match json_as_value_result {
+                Ok(json_as_value) => {
+                   let pretty_result = serde_json::to_string_pretty(&json_as_value);
+                    match pretty_result {
+                        Ok(v) => println!("Data is: {}", v),
+                        Err(e) => println!("Couldn't pretty print: {}", e),
+                    }               
+                }
+                Err(e) => println!("Couldn't parse file to Values: {}", e),
             }
         }
-        Err(e) => println!("Error: {}", e),
+        Err(e) => println!("Error: Coulding load file: {}", e),
+    }
+}
+
+/////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_json_to_values_empty_string_is_error() {
+        let result = parse_json_to_values("");
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn parse_json_to_values_simple_json_is_valid() {
+    let data = r#"{
+                    "name": "John Doe",
+                    "age": 43,
+                    "phones": [
+                      "+44 1234567",
+                      "+44 2345678"
+                    ]
+                  }"#;
+        let result = parse_json_to_values(&data);
+        assert!(result.is_ok());
     }
 }
