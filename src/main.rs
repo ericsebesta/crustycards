4b62d4b62de048513451504c564435b948827269
A B <ab@gmail.com>
2017-08-13 21:30:09 -0500

    add card struct, start

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #[macro_use]
 extern crate serde_json;
 
-use serde_json::{Value, Error};
+use serde_json::Value;
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
+
+struct Card {
+    name: String,
+    json: Value,
+}
 
 fn load_json_from_file(file_relative_path: &str) -> io::Result<String> {
     let mut f = File::open(file_relative_path)?;
@@ -20,6 +25,19 @@ fn parse_json_to_values(buffer: &str) -> Result<Value, String> {
     }
 }
 
+fn parse_values_to_cards(values: &Value) -> Result<Vec<Card>, String> {
+    let cards = Vec::new();
+    match values {
+        Null => println!("found null"),
+        Bool => println!("found null"),
+        Number => println!("found null"),
+        String => println!("found null"),
+        Array => println!("found null"),
+        Object => println!("found null"),
+    }
+    Ok(cards)
+}
+
 fn main() {
     let json_as_string_result = load_json_from_file("data/cards.collectible.json");
     match json_as_string_result {
@@ -27,11 +45,12 @@ fn main() {
             let json_as_value_result = parse_json_to_values(&json_as_string);
             match json_as_value_result {
                 Ok(json_as_value) => {
-                   let pretty_result = serde_json::to_string_pretty(&json_as_value);
+                    let pretty_result = serde_json::to_string_pretty(&json_as_value);
                     match pretty_result {
                         Ok(v) => println!("Data is: {}", v),
                         Err(e) => println!("Couldn't pretty print: {}", e),
-                    }               
+                    }
+                    let cards = parse_values_to_cards(&json_as_value);
                 }
                 Err(e) => println!("Couldn't parse file to Values: {}", e),
             }
@@ -54,7 +73,7 @@ mod tests {
 
     #[test]
     fn parse_json_to_values_simple_json_is_valid() {
-    let data = r#"{
+        let data = r#"{
                     "name": "John Doe",
                     "age": 43,
                     "phones": [
